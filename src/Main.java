87d013fc642ccee090c9cb8e6c5549a5e33af9dd
unknown <keesmark@gmail.com>
2020-01-28 09:09:10 +0900

    add average

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,13 @@ public class Main {
             String menuKind
     ) {
         double result = 0.0;
+        int recipeSize = 0;
 
         for (int i = 0; i < ms.size(); i++) {
             if (ms.get(i).getMenuKind().equals(menuKind)) {
+                recipeSize = recipeSize + ms.get(i).getRecipesLists().size();
                 result = result + ms.get(i).getRecipesLists().get(i).getCal();
+                result = result / recipeSize;
             }
         }
         return result;
